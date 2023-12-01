bf456f51c54bc8ed2cdf0d5dc3f7cd31fccda545
Barrett Whetstone <barrett.whetstone@slalom.com>
2023-12-01 17:16:50 -0500

    initial function write

--- day1/trebuchet.go
+++ day1/trebuchet.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -21,13 +22,44 @@ func main() {
 		}
 	}()
 
-	// b, err := io.ReadAll(file)
-	// fmt.Println(b)
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() { // internally, it advances token based on sperator
-		fmt.Println(scanner.Text()) // token in unicode-char
-		// fmt.Println(scanner.Bytes()) // token in bytes
+	var calSum int
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+		var firstNum string
+		var lastNum string
+		var firstNumFound bool
+
+		for index, x := range line {
+			// fmt.Println(index, string(x))
+			char := string(x)
+			_, err := strconv.Atoi(char)
+			if err != nil {
+				// not an int; skip
+				continue
+			}
+
+			if !firstNumFound {
+				firstNumFound = true
+				firstNum = char
+				lastNum = char
+			} else {
+				lastNum = char
+			}
+
+			if index == (len(line) - 1) {
+				combinedNum := firstNum + lastNum
+				fmt.Println(combinedNum)
+				num, err := strconv.Atoi(combinedNum)
+				if err != nil {
+					log.Fatal(err)
+				}
+				calSum = calSum + num
+			}
+		}
 	}
 
+	fmt.Printf("The summation of all calibration values is %x", calSum)
 }
