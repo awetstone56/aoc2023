0674905e258a03e6ed37765f102e56fb855681af
awetstone56 <awetstone56@protonmail.ch>
2023-12-03 23:15:58 -0500

    overkill for part 2

--- day1/trebuchet.go
+++ day1/trebuchet.go
@@ -5,12 +5,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
+	"strings"
 )
+
+var numMap = map[string]string{
+	"zero":  "0o",
+	"one":   "o1e",
+	"two":   "t2e",
+	"three": "t3e",
+	"four":  "4",
+	"five":  "5e",
+	"six":   "6",
+	"seven": "7n",
+	"eight": "e8t",
+	"nine":  "n9e",
+}
 
 func main() {
 	// open input file
 	file, err := os.Open("puzzleInput.txt")
+	// file, err := os.Open("smallPuzzleInput.txt") // this answer should be 585
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,12 +43,18 @@ func main() {
 	var calSum int
 	for scanner.Scan() {
 		line := scanner.Text()
-		// fmt.Print(line)
+
+		fmt.Print(line + " => ")
+		// part two logic
+		// can replace numbers spelt out with the actual number char,
+		// then go for the for loop I built for first part
+		line = replaceWordsWithNums(line)
+		fmt.Println(line)
+
 		firstNum := ""
 		lastNum := ""
 		firstNumFound := false
 		for index, x := range line {
-			// fmt.Println(index, string(x))
 			char := string(x)
 			_, err := strconv.Atoi(char)
 			if err == nil {
@@ -46,7 +68,6 @@ func main() {
 
 			if index == (len(line) - 1) {
 				combinedNum := fmt.Sprintf("%s%s", firstNum, lastNum)
-				// fmt.Println(combinedNum)
 				num, err := strconv.Atoi(combinedNum)
 				if err != nil {
 					log.Fatal(err)
@@ -59,4 +80,25 @@ func main() {
 		fmt.Printf(" = %d", calSum)
 		fmt.Println()
 	}
+}
+
+// replaceSpelledOutNumbers replaces spelled-out numbers in the input string with numeric values
+func replaceWordsWithNums(line string) string {
+
+	keys := make([]string, 0, len(numMap))
+	for k := range numMap {
+		keys = append(keys, k)
+		regexStr := "(" + strings.Join(keys, "|") + ")"
+		re := regexp.MustCompile("(" + regexStr + "|\\d+)")
+		matches := re.FindAllString(line, -1) // -1 indicates get ALL matches not a specific number of them
+		for _, match := range matches {
+			if replacementValue, exists := numMap[match]; exists {
+				// getLastChar is to deal with edge case of a letter being needed for the next match..
+				// this might break it though in other edge cases..
+				line = strings.ReplaceAll(line, match, replacementValue)
+			}
+		}
+	}
+
+	return line
 }
